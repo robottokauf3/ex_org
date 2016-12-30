888b3b2036c051079fd37cc2e5f963573ba91585
Rob Kaufmann <rok3@rok3.me>
2016-12-30 00:33:41 -0500

    Add Coveralls

--- mix.exs
+++ mix.exs
@@ -11,6 +11,8 @@ defmodule ExOrg.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
      package: package()]
   end
 
@@ -23,6 +25,7 @@ defmodule ExOrg.Mixfile do
       # Testing and documentation
       {:credo, "~> 0.5", only: [:dev, :test]},
       {:inch_ex, "~> 0.5", only: [:docs]},
+      {:excoveralls, "~> 0.5", only: [:dev, :test]},
       {:ex_doc, "~> 0.14", only: [:dev]},
       {:dialyxir, "~> 0.4.1", only: [:dev], runtime: false}
     ]
